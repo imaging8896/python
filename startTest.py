a292e69b14180043e6bf3d0a06f485ea661e11da
imaging8896 <imaging8896@gmail.com>
2016-10-16 22:10:46 +0800

    Add FuncSpec.py docTest

--- startTest.py
+++ startTest.py
@@ -6,10 +6,11 @@ Created on 2016.10.16
 
 import unittest
 import doctest
-from code import tedUtils
+from code import tedUtils, FuncSpec
     
 
 if __name__ == '__main__':
     suite = doctest.DocTestSuite()
     suite.addTests(doctest.DocTestSuite(tedUtils))
+    suite.addTests(doctest.DocTestSuite(FuncSpec))
     unittest.TextTestRunner(verbosity=2).run(suite)
